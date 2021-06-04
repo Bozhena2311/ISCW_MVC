566d1e4e21575abb855938a4cc4d9ae37a38d05e
Божена Бальжанова <bozhena.balzhanova@gmail.com>
2021-06-04 20:03:32 +0300

    Auth work

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using ISCW_MVC.Models;
+
+namespace ISCW_MVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = "data source=localhost; database=ISCW_MVC_DB; integrated security = SSPI;";
+        }
+        [HttpPost]
+
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from [User] where user_name='"+acc.UserName+"' and password='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+            
+            
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ISCW_MVC.MvcApplication" Language="C#" %>
